4bf51d9426af78295d83d09a788d1c4bfd4621ff
jerrytom0007 <jdlee@fastmail.com>
2020-04-13 09:17:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import csv
 
+// setup your file location here
 sourceFile = "C:/Users/wwwle/Desktop/book2.csv"  //replace it with the data file path
 targetFile = "C:/Users/wwwle/Desktop/book3.csv"  //replace it with the result file path
+
+// init `dict` and `list` files.
 dict1 = {}
 finalList = [[]]
+
 with open(sourceFile) as f:
+    // number each element and store the result in `dict1`
     reader = csv.reader(f)
     names = list(reader)
     for each1 in names:
@@ -15,12 +20,16 @@ with open(sourceFile) as f:
                 dict1[each2] += 1
             else:
                 dict1[each2] = 1
+                
+    // export data from `dict1` to `finalList`
     keynum = len(dict1)
     for i in range(keynum):
         (key, value) = dict1.popitem()
         finalList.append([key, value])
         keynum -= 1
     f.close()
+    
+// write results from `finalList` to targetFile.
 with open(targetFile, 'w', newline='') as f:
     writer = csv.writer(f)
     for row in finalList:
